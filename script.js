6b55702fe95993e419c071f61222382dcf482b3f
Gurman preet Singh <98319740+Gsingh2001@users.noreply.github.com>
2023-11-04 11:42:59 +0530

    second step

---
+++ script.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDOUoczvZSQG7TdglfI-lvumw9Ry8QrDAw",
+    authDomain: "organify-f7003.firebaseapp.com",
+    projectId: "organify-f7003",
+    storageBucket: "organify-f7003.appspot.com",
+    messagingSenderId: "940725476956",
+    appId: "1:940725476956:web:5e430b9ae6696689eb1099"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
